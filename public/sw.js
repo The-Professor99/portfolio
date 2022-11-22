8bff1db6cee58ee8f9c631c64619f01ba7e2f6f1
The-Professor99 <festusihechi99@gmail.com>
2022-11-22 21:39:16 +0100

    Add EHR and Lendsqr interview applications

--- public/sw.js
+++ public/sw.js
@@ -1,89 +1,91 @@
-const staticCache = 'static-site-ii-v998';
-const dynamicCache = 'dynamic-site-ii-v998';
+const staticCache = "static-site-ii-v997";
+const dynamicCache = "dynamic-site-ii-v997";
 const assets = [
-  '/offline.html',
+  "/offline.html",
 
-  '/images/icons/filled/apple-touch-icon.png',
-  '/images/icons/filled/android-chrome-512x512.png',
-  '/images/icons/white/android-chrome-512x512.png',
+  "/images/icons/filled/apple-touch-icon.png",
+  "/images/icons/filled/android-chrome-512x512.png",
+  "/images/icons/white/android-chrome-512x512.png",
 
-  '/images/static/author.png',
-  '/images/static/about.svg',
-  '/images/static/skills.svg',
-  '/images/static/resume.pdf',
+  "/images/static/author.png",
+  "/images/static/about.svg",
+  "/images/static/skills.svg",
+  "/images/static/resume.pdf",
 
-  '/fonts/Gabriola.ttf',
-  '/fonts/LibreBaskerville-Regular.ttf',
-  '/fonts/Montserrat-Regular.ttf',
-  '/fonts/Philosopher-Regular.ttf',
+  "/fonts/Gabriola.ttf",
+  "/fonts/LibreBaskerville-Regular.ttf",
+  "/fonts/Montserrat-Regular.ttf",
+  "/fonts/Philosopher-Regular.ttf",
 
-  '/css/style-1a8dcec9.css',
-  '/js/app-2d1f25ca.js',
-  '/js/main-c2c17d7d.js',
-  '/js/modules/ui-f4a83908.js',
-  'https://kit.fontawesome.com/885fbd8d84.js'
+  "/css/style-1a8dcec9.css",
+  "/js/app-2d1f25ca.js",
+  "/js/main-c2c17d7d.js",
+  "/js/modules/ui-f4a83908.js",
+  "https://kit.fontawesome.com/885fbd8d84.js",
 ];
 
 const limitCacheSize = (name, size) => {
   caches
     .open(name)
-    .then(cache => {
-      cache.keys().then(keys => {
+    .then((cache) => {
+      cache.keys().then((keys) => {
         if (keys.length > size) {
           cache.delete(keys[0]).then(limitCacheSize(name, size));
         }
       });
     })
-    .catch(error => console.log('Error while reducing files', error));
+    .catch((error) => console.log("Error while reducing files", error));
 };
 
-self.addEventListener('install', e => {
+self.addEventListener("install", (e) => {
   e.waitUntil(
     caches
       .open(staticCache)
-      .then(cache => {
+      .then((cache) => {
         return cache.addAll(assets);
       })
-      .catch(error => console.log('Error caching files', error))
+      .catch((error) => console.log("Error caching files", error))
   );
 });
 
-self.addEventListener('activate', e => {
+self.addEventListener("activate", (e) => {
   e.waitUntil(
     caches
       .keys()
-      .then(keys => {
+      .then((keys) => {
         return Promise.all(
           keys
-            .filter(key => key !== staticCache && key !== dynamicCache)
-            .map(key => caches.delete(key))
+            .filter((key) => key !== staticCache && key !== dynamicCache)
+            .map((key) => caches.delete(key))
         );
       })
-      .catch(error => console.log('Error deleting cache', error))
+      .catch((error) => console.log("Error deleting cache", error))
   );
 });
 
-self.addEventListener('fetch', e => {
-  if (!(e.request.url.indexOf('http') === 0)) return;
+self.addEventListener("fetch", (e) => {
+  if (!(e.request.url.indexOf("http") === 0)) return;
 
   e.respondWith(
     caches
       .match(e.request)
-      .then(cacheResponse => {
+      .then((cacheResponse) => {
         return (
           cacheResponse ||
-          fetch(e.request).then(fetchResponse => {
+          fetch(e.request).then((fetchResponse) => {
             return caches
               .open(dynamicCache)
-              .then(cache => {
+              .then((cache) => {
                 cache.put(e.request.url, fetchResponse.clone());
                 limitCacheSize(dynamicCache, 50);
                 return fetchResponse;
               })
-              .catch(error => console.log('Error adding file to cache', error));
+              .catch((error) =>
+                console.log("Error adding file to cache", error)
+              );
           })
         );
       })
-      .catch(() => caches.match('/offline.html'))
+      .catch(() => caches.match("/offline.html"))
   );
 });
